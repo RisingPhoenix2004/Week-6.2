bcd72e2cecd54748d62d6949b97eb0eb818297c9
Surya Alakanti <alakantisurya13@gmail.com>
2024-11-17 11:53:40 +0530

    inital commit

---
+++ src/main/java/com/example/my_maven_project/App.java
@@ -0,0 +1,7 @@
+package com.example.my_maven_project;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World! This is Surya Alakanti 22BD1A6702");
+    }
+}
